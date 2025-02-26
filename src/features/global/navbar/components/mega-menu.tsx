845d7bbdb4c4688a193b9e17cee474b8cfe871b8
melwin <114172287+itzTedx@users.noreply.github.com>
2025-02-26 21:55:55 +0400

    feat(mega-menu): update layout and styling for improved category display

--- src/features/global/navbar/components/mega-menu.tsx
+++ src/features/global/navbar/components/mega-menu.tsx
@@ -5,12 +5,12 @@ import { Category, SubCategory } from "@/payload-types";
 export const MegaMenu = ({ data }: { data: Category[] }) => {
   return (
     <div className="relative w-full md:w-[76rem] lg:w-[86rem]">
-      <div className="grid grid-cols-4 p-2">
+      <div className="columns-5 gap-3">
         {data.map((category) => (
-          <div key={category.id} className="flex flex-col gap-1 p-2">
+          <div key={category.id} className="inline-block p-2 md:mt-3">
             <Link
               href="/"
-              className="rounded-md py-1.5 text-lg font-medium transition-colors"
+              className="rounded-md border-b px-1 py-1.5 text-base transition-colors"
             >
               {category.title}
             </Link>
@@ -24,7 +24,7 @@ export const MegaMenu = ({ data }: { data: Category[] }) => {
                   <li key={sub.id}>
                     <Link
                       href={sub.slug!}
-                      className="block py-1 transition-colors hover:bg-gray-100"
+                      className="block p-1 text-sm font-medium transition-colors hover:bg-gray-100"
                     >
                       <span>{sub.title}</span>
                     </Link>
