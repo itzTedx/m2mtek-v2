1f23187baec90403f8db52ff1ef7a053f53c4e17
melwin <114172287+itzTedx@users.noreply.github.com>
2025-03-07 11:28:18 +0400

    feat(categories): rename 'categories' to 'subcategories' in product and category configurations

--- src/collections/products/index.ts
+++ src/collections/products/index.ts
@@ -212,7 +212,7 @@ export const Products: CollectionConfig = {
     },
 
     {
-      name: "categories",
+      name: "subcategories",
       type: "relationship",
       admin: {
         position: "sidebar",
