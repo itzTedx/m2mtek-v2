c1487ddfec451da52c46b692d2ca54ba8bcf1dd2
melwin <114172287+itzTedx@users.noreply.github.com>
2025-03-07 11:58:38 +0400

    feat(subcategories): update product querying to use payload and display products in subcategories page

--- src/app/(frontend)/[categories]/[subcategories]/page.tsx
+++ src/app/(frontend)/[categories]/[subcategories]/page.tsx
@@ -1,4 +1,7 @@
-import { queryProducts, querySubcategories } from "@/server/actions";
+import { payload } from "@/lib/payload";
+import { querySubcategories } from "@/server/actions";
+
+import { ProductCard } from "../../product/_components/product-card";
 
 type Params = Promise<{ subcategories: string }>;
 
@@ -10,7 +13,22 @@ export default async function SubcategoriesPage({
   const { subcategories } = await params;
 
   const [data] = await querySubcategories({ slug: subcategories });
-  const products = await queryProducts(data.id);
+  const products = await payload.find({
+    collection: "products",
+    depth: 1,
+    sort: "-createdAt",
+    pagination: false,
+    where: {
+      _status: {
+        equals: "published",
+      },
+      subcategories: {
+        equals: data.id,
+      },
+    },
+  });
+
+  if (products.docs.length === 0) return null;
 
   console.log(products);
   return (
@@ -19,7 +37,11 @@ export default async function SubcategoriesPage({
         <h1>{data.title}</h1>
         <p>{data.description}</p>
       </header>
-      <section className="container grid grid-cols-4 gap-6"></section>
+      <section className="container grid grid-cols-4 gap-6">
+        {products.docs.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
     </main>
   );
 }
